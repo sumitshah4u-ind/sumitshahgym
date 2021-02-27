ccc03da2fa95da85c29b655c01ab08d76f14d8d6
sumitshah4u-ind <79634648+sumitshah4u-ind@users.noreply.github.com>
2021-02-27 13:53:57 +0530

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -18,8 +18,8 @@ Auth::routes();
 
 Route::resource('members', 'MembersController');
 
-Route::resource('register', 'RegisterController');
-Route::resource('home', 'HomeController');
+Route::resource('register', 'Auth\RegisterController');
+Route::resource('home', 'Auth\HomeController');
 
 Route::get('/reports', 'MembersController@reportsIndex')->name('reports');
 Route::post('/reports', 'MembersController@viewReport')->name('view reports');
