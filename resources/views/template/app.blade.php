dc4dc09786c6440c39dda01f2dff0d1039523707
sumitshah4u-ind <79634648+sumitshah4u-ind@users.noreply.github.com>
2021-02-27 11:59:08 +0530

    Update app.blade.php

--- resources/views/template/app.blade.php
+++ resources/views/template/app.blade.php
@@ -15,7 +15,7 @@
   <nav class="navbar navbar-inverse">
   <div class="container-fluid">
     <div class="navbar-header">
-      <a class="navbar-brand" href="/">Gym</a>
+      <a class="navbar-brand" href="/">Sumit's Gym</a>
     </div>
     <ul class="nav navbar-nav">
       <li class="active"><a href="/">Home</a></li>
